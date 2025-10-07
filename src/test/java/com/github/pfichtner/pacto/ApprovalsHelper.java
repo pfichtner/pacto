e41534f29bd477fd660eca5049350464e30785c7
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-10-07 18:38:51 +0200

    generators added to approvals

--- src/test/java/com/github/pfichtner/pacto/ApprovalsHelper.java
+++ src/test/java/com/github/pfichtner/pacto/ApprovalsHelper.java
@@ -22,7 +22,21 @@ public final class ApprovalsHelper {
 		var root = new JsonObject();
 		root.add("body", parseString(dslFrom.toString()).getAsJsonObject());
 		root.add("matchingRules", gson.toJsonTree(new TreeMap<>(dslFrom.getMatchers().getMatchingRules())));
+		root.add("generators", new JsonPrimitive(dslFrom.getGenerators().toString()));
 		return root;
+	}
+
+	public static JsonObject scrubBodyStringArg(JsonObject json) {
+		if (json.isJsonObject()) {
+			JsonObject rootObj = json.getAsJsonObject();
+			if (rootObj.has("body") && rootObj.get("body").isJsonObject()) {
+				JsonObject body = rootObj.getAsJsonObject("body");
+				if (body.has("stringArg")) {
+					body.addProperty("stringArg", "$$stringArg$$");
+				}
+			}
+		}
+		return json;
 	}
 
 }
